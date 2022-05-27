722fa64727cdea8cd021c7eb88e3334c622daa47
axeloehrli <axel.oehrli@gmail.com>
2022-05-27 20:25:35 -0300

    Updates

---
+++ precache-manifest.5d3c5a818d21089cfa9f604fd64379eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2040b6509610b68a979c",
+    "url": "/mathematics/static/css/main.207eb754.chunk.css"
+  },
+  {
+    "revision": "2040b6509610b68a979c",
+    "url": "/mathematics/static/js/main.2040b650.chunk.js"
+  },
+  {
+    "revision": "fbbdfdcaf786c5994ac8",
+    "url": "/mathematics/static/js/1.fbbdfdca.chunk.js"
+  },
+  {
+    "revision": "1526d21a0fa9f2815031",
+    "url": "/mathematics/static/js/runtime~main.1526d21a.js"
+  },
+  {
+    "revision": "053a71700b2f43dedcbc0a4d656c7ad1",
+    "url": "/mathematics/static/media/open.053a7170.svg"
+  },
+  {
+    "revision": "f2d5641ee32a811875929f5316c45092",
+    "url": "/mathematics/static/media/close.f2d5641e.svg"
+  },
+  {
+    "revision": "272eb083a972b68712668bd72f34d6e7",
+    "url": "/mathematics/index.html"
+  }
+];
